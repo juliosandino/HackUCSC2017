84f6e61952d0fc46770d6a24a0caaa032ba9324e
rat395 <rat395@ymail.com>
2017-01-21 05:05:20 -0800

    fixed json files, updated content and manifest files

--- content.js
+++ content.js
@@ -1,1 +1,8 @@
 document.body.innerHTML = document.body.innerHTML.replace(/hello/g, 'hi');
+
+console.log(chrome.extension.getURL('test.json'));
+
+
+$.getJSON(chrome.extension.getURL('dictionary.json'), function(data) {
+   console.log(data);
+});
