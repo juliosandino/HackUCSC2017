e45d7d05098e866b75fa27b36b0521c445f55818
rat395 <rat395@ymail.com>
2017-01-21 15:27:52 -0800

    code is still crashing dynamic pages

--- content.js
+++ content.js
@@ -1,8 +1,17 @@
-document.body.innerHTML = document.body.innerHTML.replace(/hello/g, 'hi');
 
 console.log(chrome.extension.getURL('test.json'));
 
+$(document).ready(function() {
 
-$.getJSON(chrome.extension.getURL('dictionary.json'), function(data) {
-   console.log(data);
+    // setTimeout(function(){
+    //     document.body.innerHTML = document.body.innerHTML.replace(/hello/g, 'hi');
+    // }, 500);
+
+    $.getJSON(chrome.extension.getURL('dictionary.json'), function(data) {
+       $.each (data, function (key, val) {
+        var replaced = $("body").html().replace(new RegExp(key.toString(), "g"), val.toString());
+        $("body").html(replaced);
+       });
+    });
 });
+
